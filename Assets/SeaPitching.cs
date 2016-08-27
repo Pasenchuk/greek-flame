ca2217202dc6144cdf1e96ced5b62bcf01cfb793
Pasenchuk Victor <vpasenchuk@thumbtack.net>
2016-08-28 05:55:59 +0600

    Add sea pitching

---
+++ Assets/SeaPitching.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SeaPitching : MonoBehaviour
+{
+
+	private const float MAX_ANGLE = 5;
+
+	private float angle = 0;
+	private bool direction = true;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (direction) {
+			angle += Time.deltaTime;
+			if (angle > MAX_ANGLE + Random.Range(0f, 3f))
+				direction = false;
+		} else {
+			angle -= Time.deltaTime;
+			if (angle < -MAX_ANGLE - Random.Range(0f, 3f))
+				direction = true;
+		}
+
+		transform.localEulerAngles = new Vector3 (angle, 0, 0);
+	}
+}
